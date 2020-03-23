0c5ebf9a23b0288303bd2c5e6853fd507823733a
veeksssha <vhorobko@gmail.com>
2020-03-23 10:41:31 +0100

    Последние правки перед второй защитой

--- js/script.js
+++ js/script.js
@@ -7,25 +7,9 @@ var form = popup.querySelector("form");
 var name = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
 
-var isStorageSupport = true;
-var storage = "";
-
-try {
-	storage = localStorage.getItem("name");
-} catch (err) {
-	isStorageSupport = false;
-}
-
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
-		
-	if (storage) {
-		name.value = storage;
-		email.focus();
-	} else {
-		name.focus();
-	}
 });
 
 close.addEventListener("click", function (evt) {
@@ -40,10 +24,6 @@ form.addEventListener("submit", function (evt) {
 		popup.classList.remove("modal-error");
 		popup.offsetWidth = popup.offsetWidth;
 		popup.classList.add("modal-error");
-	} else {
-		if (isStorageSupport) {
-			localStorage.setItem("name", name.value);
-		}
 	}
 });
 
